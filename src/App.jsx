2a8ae5544839bf8acd2c8b365497d616f5ba156d
Apple <apple@Apples-MacBook-Air.local>
2026-01-03 10:24:22 +0530

    second commit memo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,46 @@
-import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+// memo code
 
-import React from "react";
-import { CounterAtom } from "./store/atoms/counter";
+/* this is the way of protecting the re-rendering in the children that not using the   */
+
+import { memo, useState, useEffect } from "react";
 
 function App() {
   return (
     <>
-      <RecoilRoot>
-        <Counter />
-        <Increase />
-        <Decrease />
-      </RecoilRoot>
-      <div>Hello Counter</div>
+      <Counter />
     </>
   );
 }
 
 function Counter() {
-  const count = useRecoilValue(CounterAtom);
-   console.log("count:", count);
+  const [count, setcount] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setcount((c) => c + 1);
+    }, 3000);
+    return () => clearInterval(interval); // Good practice: cleanup your intervals!
+  }, []);
+  return (
+    <>
+      <CurrentCount count={count} />
+      <Increase />
+      <Decrease />
+    </>
+  );
+}
+
+const CurrentCount = memo(({ count }) => {
+  console.log("Current Count is rendering.")
   return <div>{count}</div>;
-}
+});
 
-function Increase() {
-  const setcount = useSetRecoilState(CounterAtom);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setcount((c) => c + 1);
-        }}
-      >
-        Increase
-      </button>
-    </div>
-  );
-}
+const Increase = memo(() => {
+  console.log("increase rendered");
+  return <div><button>increase</button></div>;
+});
 
-function Decrease() {
-  const setcount = useSetRecoilState(CounterAtom);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setcount((c) => c - 1);
-        }}
-      >
-        Decrease
-      </button>
-    </div>
-  );
-}
- export default App;
-
-
-// import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
-// import { CounterAtom } from "./store/atoms/counter";
-
-// function App() {
-//   return (
-//     <RecoilRoot>
-//       <Counter />
-//     </RecoilRoot>
-//   );
-// }
-
-// function Counter() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "40px" }}>
-//       <CurrentCount />
-//       <Increase />
-//       <Decrease />
-//     </div>
-//   );
-// }
-
-// function CurrentCount() {
-//   const count = useRecoilValue(CounterAtom);
-//   return <h2>Count: {count}</h2>;
-// }
-
-// function Increase() {
-//   const setCount = useSetRecoilState(CounterAtom);
-//   return <button onClick={() => setCount(c => c + 1)}>Increase</button>;
-// }
-
-// function Decrease() {
-//   const setCount = useSetRecoilState(CounterAtom);
-//   return <button onClick={() => setCount(c => c - 1)}>Decrease</button>;
-// }
-
-// export default App;
+const Decrease = memo(() => {
+  console.log("decrease rendered");
+  return <div><button>decrease</button></div>;
+});
+export default App;
